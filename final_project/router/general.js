d2ee0b6cae86f1c733b62512c79297dccfe09cf6
neethuk88 <neetuk88@gmail.com>
2025-03-18 12:00:19 -0400

    changes made to register route

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,38 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
+
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
@@ -71,7 +100,16 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; // Retrieve the ISBN from the request parameters
+    const booksArray = Object.values(books); // Convert books object to an array
+    const book = booksArray.find(b => b.isbn === isbn); // Find the book with the matching ISBN
+
+    if (book && book.reviews) {
+        res.json(book.reviews); // Send the book reviews as a JSON response
+    } else {
+        res.status(404).json({ message: "Reviews not found for this book" }); // Send a 404 response if reviews are not found
+    }
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
