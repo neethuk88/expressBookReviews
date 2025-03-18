025e3b1719edf18c1f211305d29c9583fcfa27d9
neethuk88 <neetuk88@gmail.com>
2025-03-18 12:57:27 -0400

    changes made to add and delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,11 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let filtered_users = users.filter((user)=> user.username === user);
+    if(filtered_users){
+        return true;
+    }
+    return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
@@ -52,8 +57,22 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let userd = req.session.username;
+  let ISBN = req.params.isbn;
+  let details = req.query.review;
+  let rev = {user:userd,review:details}
+  books[ISBN].reviews = rev;
+  return res.status(201).json({message:"Review added successfully"})
+  
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let ISBN = req.params.isbn;
+    books[ISBN].reviews = {}
+    return res.status(200).json({messsage:"Review has been deleted"})
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
